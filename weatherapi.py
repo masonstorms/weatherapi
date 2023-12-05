c799ee9eba85532ae781a17e342a5b81d451c468
Mason Storms <billowillo@users.noreply.github.com>
2023-12-05 10:45:07 -0700

    Initial Commit

---
+++ weatherapi.py
@@ -0,0 +1,35 @@
+import requests
+
+def get_weather(api_key, city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": api_key,
+        "units": "imperial"  # You can change units to imperial or metric
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if response.status_code == 200:
+            weather_description = data["weather"][0]["description"]
+            temperature = data["main"]["temp"]
+            humidity = data["main"]["humidity"]
+            wind_speed = data["wind"]["speed"]
+
+            print(f"Weather in {city}:")
+            print(f"Description: {weather_description}")
+            print(f"Temperature: {temperature}°C")
+            print(f"Humidity: {humidity}%")
+            print(f"Wind Speed: {wind_speed} m/s")
+        else:
+            print(f"Error: {data['message']}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    import config
+    api_key = config.api_key
+    city = input("Enter the city name: ")
+    get_weather(api_key, city)
